eeb2c251d1fa40e0479371a47aeca6976675dbca
Audrey Terrien <aterrien.education@gmail.com>
2024-05-13 11:49:40 +0200

    Add .env

--- lib/__init__.py
+++ lib/__init__.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 #import sys
 # Importation des variables et des modules personnels
 load_dotenv()
-from .module_eda import *
-from .module_preprocess import *
-from .module_visual_report import *
-from .module_text_eda import *
-from .module_visual_results import *
+#from module_eda import missing, info_table, num_graph, categorical_graph_full, distribution_entre_deux_cat, wordcloud_plot
+#from .module_preprocess import *
+#from .module_visual_report import *
+#from .module_text_eda import *
+#from .module_visual_results import *
 
 # Format basé sur les sources suivantes:
 # PEP 8 – Style Guide for Python Code (https://peps.python.org/pep-0008/)
